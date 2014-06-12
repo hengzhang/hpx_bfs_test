7685a538bc37dbd61cbb2f75276b8ae05401eaaa
HengZhang <zhangheng@nfs.iscas.ac.cn>
2014-06-12 10:56:23 +0800

    client modify
    client modify

--- bfs_simple/bfs_simple_client.cpp
+++ bfs_simple/bfs_simple_client.cpp
@@ -16,25 +16,9 @@
 #include <fstream>
 #include <iostream>
 
-/*void matrix_to_adjlist(std::size_t *matrix, adjNode *adjList, int n){ 
-  adjNode *tempNode;
-  for(int i = 0;i<n;++i){//row n
-    adjList[i].node = i;
-    adjList[i].next = NULL;
-
-    for(int j = n-1;j>=0;j--){//column n
-      if(*(matrix+i*n+j) == 1){ 
-        tempNode = (adjNode *) malloc(sizeof(adjNode));
-        tempNode->next = adjList[i].next;
-        tempNode->node = j;
-        adjList[i].next = tempNode;
-      }   
-    }   
-  }
-}*/
-inline void init(hpx::components::server::distributing_factory::iterator_range_type r,  std::vector<bfs_simple::point>& accu){
+inline void init(hpx::components::server::distributing_factory::iterator_range_type r,  std::vector<hpx::naming::id_type>& accu){
   BOOST_FOREACH(hpx::naming::id_type const& id, r)
-    accu.push_back(bfs_simple::point());
+    accu.push_back(id);
 }
 
 int hpx_main(boost::program_options::variables_map& vm){
@@ -43,11 +27,8 @@ int hpx_main(boost::program_options::variables_map& vm){
     std::size_t const num = vm["n"].as<std::size_t>();
     std::size_t const root = vm["root"].as<std::size_t>();
     std::size_t const max_levels = vm["max-levels"].as<std::size_t>();
-    std::size_t const max_num_neighbors
-      = vm["max-num-neighbors"].as<std::size_t>();
     std::string const edge_file = vm["graph"].as<std::string>();
 
-    //   std::size_t nodes[num]={0};
     std::vector<std::vector<std::size_t> >  node_neighbors(num);
 
     std::string line;
@@ -67,89 +48,80 @@ int hpx_main(boost::program_options::variables_map& vm){
         }
       }
     }
-    hpx::components::distributing_factory factory;
- //   factory.create(hpx::applier::get_applier().get_runtime_support_gid());  
-    factory.create(hpx::find_here());
-  //  std::vector<hpx::id_type> localities = hpx::find_all_localities();   
+    for(std::size_t i =0 ;i< num; i++){
+      std::cout<<"node "<<i<<":";
+      for(std::vector<std::size_t>::iterator it = node_neighbors[i].begin();it != node_neighbors[i].end();++it)
+        std::cout<<" "<<*it;
+      std::cout<<'\n';
+    }
 
+    hpx::components::distributing_factory factory =  hpx::components::distributing_factory::create(hpx::find_here());
+    std::vector<hpx::id_type> localities = hpx::find_all_localities();   
 
     hpx::components::component_type block_type = 
-     hpx::components::get_component_type<bfs_simple::server::point>();
+      bfs_simple::server::point::get_component_type();
 
     hpx::components::distributing_factory::result_type blocks =
       factory.create_components(block_type,  num);
 
-    std::vector<bfs_simple::point> points;
-    //BOOST_FOREACH(hpx::naming::id_type const& id, localities)
+    std::vector<hpx::naming::id_type> points;
     ::init(hpx::util::locality_results(blocks), points);
-    
+    std::vector<hpx::lcos::future<void> > init_phase;
+    bfs_simple::server::point::init_action init;
     for(std::size_t i = 0; i< num; i++)
     {
-     // points.push_back(bfs_simple::point(hpx::components::new_<std::size_t>id, i, neighbors[i]));
-      points[i].init(i, node_neighbors[i]);
+      init_phase.push_back(hpx::async(init, points[i], i, node_neighbors[i]));
     }
+    hpx::wait_all(init_phase);
 
-    std::vector<hpx::lcos::future<std::vector<std::size_t> > > traverse_phase;
     std::size_t level = 0; 
     std::size_t parent = 9999; 
     std::vector<std::vector<std::size_t> > parents;
+    std::vector<hpx::lcos::future<std::vector<std::size_t> > > traverse_phase;
+    bfs_simple::server::point::traverse_action traverse;
     for (std::size_t i=0;i<max_levels;i++) {
       parents.push_back(std::vector<std::size_t>());
     }
     std::vector<std::vector<std::size_t> > neighbors,alt_neighbors;
     parents[level].push_back( root ); 
-    traverse_phase.push_back( points[root].traverse_async(level,parent) );
+    traverse_phase.push_back(hpx::async(traverse, points[root], level, parent));
 
     //hpx::lcos::wait(traverse_phase,neighbors);
     hpx::lcos::wait(traverse_phase, 
         [&](std::size_t, std::vector<std::size_t> t){
           neighbors.push_back(t);
         });
+//    std::cout<<"neighbors:neighbors.size,"<<neighbors.size()<<std::endl;
 
     for (std::size_t k=1;k<max_levels;k++) {
       traverse_phase.resize(0);
-
       if ( (k+1)%2 == 0 ) {
         alt_neighbors.resize(0);
         for (std::size_t i=0;i<neighbors.size();i++) {
           parent = parents[k-1][i];
           for (std::size_t j=0;j<neighbors[i].size();j++) {
             parents[k].push_back( neighbors[i][j] ); 
-            traverse_phase.push_back(points[ neighbors[i][j] ].traverse_async(k,parent));
+            traverse_phase.push_back(hpx::async(traverse, points[root], level, parent));
           } 
         }
         //hpx::lcos::wait(traverse_phase,alt_neighbors);
-        //hpx::wait_all(traverse_phase);
         hpx::lcos::wait(traverse_phase, 
             [&](std::size_t, std::vector<std::size_t> t){
               alt_neighbors.push_back(t);
         });
-
-
       } else {
         neighbors.resize(0);
         for (std::size_t i=0;i<alt_neighbors.size();i++) {
           parent = parents[k-1][i];
           for (std::size_t j=0;j<alt_neighbors[i].size();j++) {
             parents[k].push_back( alt_neighbors[i][j] ); 
-            traverse_phase.push_back(points[ alt_neighbors[i][j] ].traverse_async(k,parent));
+            traverse_phase.push_back(hpx::async(traverse, points[root], level, parent));
           } 
         }
-     /*   std::vector<hpx::lcos::future<void> > barrier;
-        for (std::size_t i=0;i<iterations;i++) {
-          std::size_t rn = rand() % array_size;
-          barrier.push_back(accu[rn].add_async()); 
-        }
-        hpx::wait_all(barrier);  
-        */
-       //hpx::lcos::wait(traverse_phase,neighbors);
-       // hpx::wait_all(traverse_phase);
         hpx::lcos::wait(traverse_phase, 
             [&](std::size_t, std::vector<std::size_t> t){
               neighbors.push_back(t);
         });
-
-
       }
     }
     std::cout << "Elapsed time: " << t.elapsed() << " [s]" << std::endl;
@@ -158,7 +130,6 @@ int hpx_main(boost::program_options::variables_map& vm){
   return hpx::finalize(); // Initiate shutdown of the runtime system.
 }
 
-
 int main(int argc, char* argv[]){
   using boost::program_options::value;
   // Configure application-specific options.
@@ -166,10 +137,8 @@ int main(int argc, char* argv[]){
     desc_commandline("Usage: " HPX_APPLICATION_STRING " [options]");
 
   desc_commandline.add_options()
-    ("n", value<std::size_t>()->default_value(100),
+    ("n", value<std::size_t>()->default_value(15),
      "the number of nodes in the graph")
-    ("max-num-neighbors", value<std::size_t>()->default_value(20),
-     "the maximum number of neighbors")
     ("max-levels", value<std::size_t>()->default_value(20),
      "the maximum number of levels to traverse")
     ("root", value<std::size_t>()->default_value(0),
